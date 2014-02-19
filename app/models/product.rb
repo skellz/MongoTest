368bc13d70032e86eeb06a2588b2769e3c9b078a
skellz <sherwin.kelly@gmail.com>
2014-02-19 00:00:43 -0500

    validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :name, type: String
   field :description, type: String
   field :price, type: Float
+
+  validates_presence_of :name, :description, :price
 end
